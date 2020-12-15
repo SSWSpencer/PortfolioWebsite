00e61397de633c7b729572fe4fe202f7ccb85ad7
Steven Spencer <steve35spencer@gmail.com>
2020-12-15 15:21:06 -0500

    Added appropriate links for py script, fixed previously unseen issue with Windows 10 default scaling, added title, desc, and site icon

--- portfolio/src/components/Downloadables.js
+++ portfolio/src/components/Downloadables.js
@@ -28,8 +28,8 @@ const Downloadables = () => {
     const projArr = [
         {
             title: "VST-Key Auto Presets",
-            download: "https://saltyhacker.now.sh/",
-            github: "https://github.com/Build-Week-Saltiest-Hack-News-Trolls-2/front-end",
+            download: "https://www.mediafire.com/file/7cdz5jzqvs2e3ti/VST-Keys.zip/file",
+            github: "https://github.com/SSWSpencer/VST-Key-Auto-Presets",
             description: "Bias FX 2 is one of the most well-known guitar amp simulators/VSTs. Although coming in at a premium price of up to $300 (and sometimes more), it doesn't come with the ability to easily switch between tones without spending extra money on a MIDI controller. While running this script at the same time as Bias FX 2, it allows the user to switch between presets using only their keyboard",
             myWork: "Many people have asked for this exact functionality in the applciation, to which the developers said it wouldn't happen. I wrote this script using both the PyAutoGui and Keyboard modules in Python. The script waits for a keyboard input (1-8), and then searches the screen for the corresponding preset button, and clicks it. This allows for quick transitions between tones while mid-play.",
             photo: VSTKeyPhoto,
